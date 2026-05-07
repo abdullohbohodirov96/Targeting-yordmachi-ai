1b1b35593c8da08b8dfdb1fa8bf22bb79ce9ca6e
Abdulloh <abdulloh@Abdullohs-MacBook-Air.local>
2026-05-07 21:01:38 +0500

    Update config to use BOT_TOKEN and OPENAI_API_KEY

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-# load .env file
 load_dotenv()
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 GROUP_ID = os.getenv("GROUP_ID")
+
 META_ACCESS_TOKEN = os.getenv("META_ACCESS_TOKEN")
 META_AD_ACCOUNT_ID = os.getenv("META_AD_ACCOUNT_ID")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
